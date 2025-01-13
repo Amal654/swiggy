3d963dba109814118613d59b34cb25c6b152dea8
mharidurgaprasad <153426039+mharidurgaprasad@users.noreply.github.com>
2025-01-13 19:21:09 +0530

    added new project inside

---
+++ swiggy-2/assets/script.js
@@ -0,0 +1,19 @@
+let scrollContainer = document.querySelector(".slider");
+
+let backBtn = document.getElementById('backBtn');
+let nextBtn = document.getElementById('nextBtn');
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltay
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+});
